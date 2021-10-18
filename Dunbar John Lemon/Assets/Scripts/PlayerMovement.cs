bb1671c2a62ff30aec8ea34e92f7ec7000cfda69
Jamie Dunbar <jpdunbar@iu.edu>
2021-10-18 16:45:56 -0400

    Added building layouts

--- Dunbar John Lemon/Assets/Scripts/PlayerMovement.cs
+++ Dunbar John Lemon/Assets/Scripts/PlayerMovement.cs
@@ -63,7 +63,7 @@ public class PlayerMovement : MonoBehaviour
 
     void OnAnimatorMove()
     {
-        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude*2);
         m_Rigidbody.MoveRotation(m_Rotation);
     }
 }
